2d70827675a546e297dccbfc93ee864feace672c
Iveth <ivethp62@gmail.com>
2025-05-15 20:42:21 -0500

    FIX

--- src/routes/priceProducts.js
+++ src/routes/priceProducts.js
@@ -6,7 +6,7 @@ const router = Router();
 
 router.post('/', authenticateToken, priceProductController.createPriceProduct);
 router.get('/:id', authenticateToken, priceProductController.getPriceProduct);
-router.get('/', authenticateToken, priceProductController.getPriceProducts);
+router.get('/', priceProductController.getPriceProducts);
 router.put('/:id', authenticateToken, priceProductController.updatePriceProduct);
 router.delete('/:id', authenticateToken, priceProductController.deletePriceProduct);
 
